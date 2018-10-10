e83d1ecd93dd3781cc7e5a404d8264b58776f049
marcelooliveira <mclricardo@gmail.com>
2018-10-10 17:16:27 -0300

    .

--- 01.02/depois/LojaDeFilmes.cs
+++ 01.02/depois/LojaDeFilmes.cs
@@ -1,15 +1,11 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace _01._02
 {
-    [JsonObject("MovieStore")]
     public class LojaDeFilmes
     {
-        [JsonProperty("Directors")]
         public List<Diretor> Diretores = new List<Diretor>();
-        [JsonProperty("Movies")]
         public List<Filme> Filmes = new List<Filme>();
         public static void AdicionarFilme(Filme filme)
         {
@@ -17,23 +13,16 @@ namespace _01._02
         }
     }
 
-    [JsonObject("Director")]
     public class Diretor
     {
-        [JsonProperty("Name")]
         public string Nome { get; set; }
-        [JsonIgnore]
         public int NumeroFilmes;
     }
 
-    [JsonObject("Movie")]
     public class Filme
     {
-        [JsonProperty("Director")]
         public Diretor Diretor { get; set; }
-        [JsonProperty("Title")]
         public string Titulo { get; set; }
-        [JsonProperty("Year")]
         public string Ano { get; set; }
     }
 }
