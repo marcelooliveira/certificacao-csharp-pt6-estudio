c84ff2c78fe538b447d0a9c4314a9478eb763ece
marcelooliveira <mclricardo@gmail.com>
2018-10-15 19:29:54 -0300

    1027-CertificacaoCSharpParte6-video4.1-Arrays

--- 02.01/depois/Program.cs
+++ 02.01/depois/Program.cs
@@ -6,7 +6,37 @@ namespace _02._01
     {
         static void Main(string[] args)
         {
+            string alura = "Alura";
+            string caelum = "Caelum";
+            string casaDoCodigo = "Casa do Código";
 
+            Console.WriteLine(alura);
+            Console.WriteLine(caelum);
+            Console.WriteLine(casaDoCodigo);
+
+            //string[] empresas = new string[3];
+            //empresas[0] = alura;
+            //empresas[1] = caelum;
+            //empresas[2] = casaDoCodigo;
+
+            //string[] empresas = new string[]
+            //{
+            //    alura, caelum, casaDoCodigo
+            //};
+
+            //string[] empresas = { alura, caelum, casaDoCodigo };
+
+            var empresas = new string[] { alura, caelum, casaDoCodigo };
+
+            for (int i = 0; i < empresas.Length; i++)
+            {
+                Console.WriteLine(empresas[i]);
+            }
+
+            foreach (var empresa in empresas)
+            {
+                Console.WriteLine(empresa);
+            }
         }
     }
 }
