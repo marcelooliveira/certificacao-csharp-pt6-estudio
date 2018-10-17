51459475b4dd6a737efef291299dd51a28cd24d7
marcelooliveira <mclricardo@gmail.com>
2018-10-17 13:43:30 -0300

    1027-CertificacaoCSharpParte6-video4.1-Arrays.trec

--- 02.01/depois/Program.cs
+++ 02.01/depois/Program.cs
@@ -6,7 +6,29 @@ namespace _02._01
     {
         static void Main(string[] args)
         {
-                                                               
+            string alura = "Alura";
+            string caelum = "Caelum";
+            string casaDoCodigo = "Casa do Código";
+
+            Console.WriteLine(alura);
+            Console.WriteLine(caelum);
+            Console.WriteLine(casaDoCodigo);
+
+            //MATRIZ = ARRAY
+
+            //string[] empresas = new string[3];
+            //empresas[0] = alura;
+            //empresas[1] = caelum;
+            //empresas[2] = casaDoCodigo;
+
+            //string[] empresas = new string[]
+            //{
+            //    alura, caelum, casaDoCodigo
+            //};
+
+            string[] empresas = { alura, caelum, casaDoCodigo };
+
+
             ///< image url="$(ProjectDir)\img6.png"/>
         }
     }
