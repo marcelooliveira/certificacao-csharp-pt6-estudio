6c40da8d9c76ba8ea462ceef24012a7287e3c05f
marcelooliveira <mclricardo@gmail.com>
2018-10-17 14:07:28 -0300

    1027-CertificacaoCSharpParte6-video4.2-Acessando Elementos do Array.trec

--- 02.01/depois/Program.cs
+++ 02.01/depois/Program.cs
@@ -28,7 +28,27 @@ namespace _02._01
 
             string[] empresas = { alura, caelum, casaDoCodigo };
 
+            Imprimir(empresas);
 
+            caelum = "Caelum Ensino e Inovação";
+
+            Imprimir(empresas);
+
+            empresas[1] = "Caelum Ensino e Inovação";
+
+            Imprimir(empresas);
+
+            Console.WriteLine("Primeiro elemento: " + empresas[0]);
+            Console.WriteLine("Último elemento: " + empresas[empresas.Length - 1]);
+        }
+
+        private static void Imprimir(string[] empresas)
+        {
+            for (int i = 0; i < empresas.Length; i++)
+            {
+                string empresa = empresas[i];
+                Console.WriteLine(empresa);
+            }
         }
     }
     ///< image url="$(ProjectDir)\img6.png"/>
