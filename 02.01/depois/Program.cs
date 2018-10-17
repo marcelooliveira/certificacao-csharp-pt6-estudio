28d635e12800718da62c5cfbe521e192e0e0caea
marcelooliveira <mclricardo@gmail.com>
2018-10-17 14:15:26 -0300

    .

--- 02.01/depois/Program.cs
+++ 02.01/depois/Program.cs
@@ -16,24 +16,30 @@ namespace _02._01
 
             //MATRIZ = ARRAY
 
+            //Forma de inicialização com tamanho
             //string[] empresas = new string[3];
             //empresas[0] = alura;
             //empresas[1] = caelum;
             //empresas[2] = casaDoCodigo;
 
+            //Forma de inicialização com tamanho automático
             //string[] empresas = new string[]
             //{
             //    alura, caelum, casaDoCodigo
             //};
 
+            //Forma de inicialização simplificada
             string[] empresas = { alura, caelum, casaDoCodigo };
 
+            //Imprime cada um dos elementos
             Imprimir(empresas);
 
+            //alterar a variável não afeta o valor do elemento do array!
             caelum = "Caelum Ensino e Inovação";
 
             Imprimir(empresas);
 
+            //mudando valor de um elemento do array
             empresas[1] = "Caelum Ensino e Inovação";
 
             Imprimir(empresas);
