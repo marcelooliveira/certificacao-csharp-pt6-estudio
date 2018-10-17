c96b7eb5ae9c7850ce102d37b2696b04909074bf
marcelooliveira <mclricardo@gmail.com>
2018-10-17 13:25:13 -0300

    .

--- 02.01/depois/Program.cs
+++ 02.01/depois/Program.cs
@@ -6,30 +6,6 @@ namespace _02._01
     {
         static void Main(string[] args)
         {
-            string alura = "Alura";
-            string caelum = "Caelum";
-            string casaDoCodigo = "Casa do Código";
-
-            //MATRIZ = ARRAY 
-
-            string[] empresas = new string[3];
-            empresas[0] = alura;
-            empresas[1] = caelum;
-            empresas[2] = casaDoCodigo;
-
-            //Console.WriteLine(alura);
-            //Console.WriteLine(caelum);
-            //Console.WriteLine(casaDoCodigo);
-
-            for (int i = 0; i < empresas.Length; i++)
-            {
-                Console.WriteLine(empresas[i]);
-            }
-
-            //primeiro elemento:
-            Console.WriteLine("O primeiro elemento é:" + empresas[0]);
-            //último elemento:
-            Console.WriteLine("O último elemento é:" + empresas[empresas.Length - 1]);
                                                                
             ///< image url="$(ProjectDir)\img6.png"/>
         }
