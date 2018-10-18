4a331658013b60499999c5af385d96874e184ce9
Marcelo Oliveira <mclricardo@gmail.com>
2018-10-18 19:26:21 -0300

    1027-CertificacaoCSharpParte6-video5.2-Adicionando e Inserindo na Lista.trec

--- 02.02/depois/Program.cs
+++ 02.02/depois/Program.cs
@@ -32,23 +32,52 @@ namespace _02._02
 
             Imprimir(empresas);
 
+            //PROBLEMA: Pegando o primeiro elemento
+            //SOLUÇÃO: Usar índice, como no array
+
+            Console.WriteLine("O primeiro elemento é: " + empresas[0]);
+
+            //PROBLEMA: Pegando o último elemento
+            //SOLUÇÃO: Usar índice, como no array
+
+            Console.WriteLine("O último elemento é: " + empresas[empresas.Count - 1]);
+
+            //PROBLEMA: modificar elemento numa certa posição
+            //SOLUÇÃO: acessar elemento pelo índice e atribuir novo valor
+
+            empresas[0] = "Alura Cursos Online";
+            Imprimir(empresas);
+
+            //PROBLEMA: Reverter a sequência da lista
+            //SOLUÇÃO: Usar método Reverse()
+            empresas.Reverse();
+            Imprimir(empresas);
+            empresas.Reverse();
+
+
+            //PROBLEMA: Adicionar um novo elemento ao final
+            //SOLUÇÃO: Usar método Add()
+            empresas.Add("MusicDot");
+            Imprimir(empresas);
+
+            //PROBLEMA: Adicionar vários elementos ao final
+            //SOLUÇÃO: Usar método AddRange()
+            var novasEmpresas = new List<string> { "empresa 1", "empresa 2" };
+            empresas.AddRange(novasEmpresas);
+            Imprimir(empresas);
+
+            //PROBLEMA: adicionar novo elemento na segunda posição
+            //SOLUÇÃO: Usar método Insert
+            var posicao = 2;
+            empresas.Insert(posicao - 1, "Hipsters.Jobs");
+            Imprimir(empresas);
+
+            //PROBLEMA: adicionar mais de um elemento na terceira posição
+            //SOLUÇÃO: Usar método InsertRange()
+            empresas.InsertRange(2, new List<string> { "empresa 3", "empresa 4" });
+            Imprimir(empresas);
+
             #region problemas
-            ///PROBLEMA: Pegando o primeiro elemento
-            ///SOLUÇÃO: Usar índice, como no array
-            ///PROBLEMA: Pegando o último elemento
-            ///SOLUÇÃO: Usar índice, como no array
-            ///PROBLEMA: modificar elemento numa certa posição
-            ///SOLUÇÃO: acessar elemento pelo índice e atribuir novo valor
-            ///PROBLEMA: Reverter a sequência da lista
-            ///SOLUÇÃO: Usar método Reverse()
-            ///PROBLEMA: Adicionar um novo elemento ao final
-            ///SOLUÇÃO: Usar método Add()
-            ///PROBLEMA: Adicionar vários elementos ao final
-            ///SOLUÇÃO: Usar método AddRange()
-            ///PROBLEMA: adicionar novo elemento na segunda posição
-            ///SOLUÇÃO: Usar método Insert
-            ///PROBLEMA: adicionar mais de um elemento na terceira posição
-            ///SOLUÇÃO: Usar método InsertRange()
             ///PROBLEMA: Ordenar elementos
             ///SOLUÇÃO: usar método sort
             ///PROBLEMA: Ordenar elementos por ordem de tamanho de string
