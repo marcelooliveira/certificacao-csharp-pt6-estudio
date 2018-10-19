93528928a2bfcde1502ccc537caf57a5ff69180d
marcelooliveira <mclricardo@gmail.com>
2018-10-19 15:10:34 -0300

    .

--- 02.02/depois/Program.cs
+++ 02.02/depois/Program.cs
@@ -5,113 +5,136 @@ namespace _02._02
 {
     class Program
     {
+        //Cronologia Star Wars
+        //=========================================
+        //Episódio I: A Ameaça Fantasma         1999
+        //Episódio II: Ataque dos Clones        2002
+        //A Guerra dos Clones                   2003
+        //Episódio III: A Vingança dos Sith     2005
+        //Rebels                                2014
+        //Rogue One                             2016
+        //Episódio IV -Uma nova esperança       1977
+        //Episódio V -O Império Contra-Ataca    1980
+        //Episódio VI -O Retorno de Jedi        1983
+        //Episódio VII -O Despertar da Força    2015
+        //Episódio VIII: Os Últimos Jedi        2017
         static void Main(string[] args)
         {
-            //declarando as variáveis iniciais
-            string alura = "Alura";
-            string caelum = "Caelum";
-            string casaDoCodigo = "Casa do Código";
+            var esperanca = new Filme("Episódio IV -Uma nova esperança", 1977);
+            var imperio = new Filme("Episódio V -O Império Contra-Ataca", 1980);
+            var retorno = new Filme("Episódio VI -O Retorno de Jedi", 1983);
+            var ameaca = new Filme("Episódio I: A Ameaça Fantasma", 1999);
+            var ataque = new Filme("Episódio II: Ataque dos Clones", 2002);
+            var guerraClones = new Filme("A Guerra dos Clones", 2003);
+            var vinganca = new Filme("Episódio III: A Vingança dos Sith", 2005);
+            var rebels = new Filme("Rebels", 2014);
+            var despertar = new Filme("Episódio VII -O Despertar da Força", 2015);
+            var rogue = new Filme("Rogue One", 2016);
+            var ultimo = new Filme("Episódio VIII: Os Últimos Jedi", 2017);
 
-            //PROBLEMA: começar com um array vazio
-            //SOLUÇÃO: trocar um array por uma lista
-            List<string> empresas = new List<string>();
+            List<Filme> cronologia = new List<Filme>();
+            Console.WriteLine("cronologia.Capacity: " + cronologia.Capacity);
 
-            //PROBLEMA: adicionar mais elementos
-            //SOLUÇÃO: usar o método Add
-            empresas.Add(alura);
-            empresas.Add(caelum);
-            empresas.Add(casaDoCodigo);
+            cronologia.Add(esperanca);
 
-            //PROBLEMA: começando com uma lista já populada
-            //SOLUÇÃO: a mesma do array
+            cronologia.AddRange(new[] { imperio, retorno });
 
-            empresas = new List<string> { alura, caelum, casaDoCodigo };
+            //List<Filme> cronologia = new List<Filme>()
+            //{
+            //    esperanca,
+            //    imperio,
+            //    retorno,
+            //};
 
-            //PROBLEMA: imprimir a lista
-            //SOLUÇÃO: varrer os elementos da lista
+            Console.WriteLine("cronologia.Capacity: " + cronologia.Capacity);
 
-            Imprimir(empresas);
+            Imprimir(cronologia);
 
-            //PROBLEMA: Pegando o primeiro elemento
-            //SOLUÇÃO: Usar índice, como no array
-
-            Console.WriteLine("O primeiro elemento é: " + empresas[0]);
-
-            //PROBLEMA: Pegando o último elemento
-            //SOLUÇÃO: Usar índice, como no array
-
-            Console.WriteLine("O último elemento é: " + empresas[empresas.Count - 1]);
-
-            //PROBLEMA: modificar elemento numa certa posição
-            //SOLUÇÃO: acessar elemento pelo índice e atribuir novo valor
-
-            empresas[0] = "Alura Cursos Online";
-            Imprimir(empresas);
-
-            //PROBLEMA: Reverter a sequência da lista
-            //SOLUÇÃO: Usar método Reverse()
-            empresas.Reverse();
-            Imprimir(empresas);
-            empresas.Reverse();
+            cronologia.Insert(0, ameaca);
+            Imprimir(cronologia);
 
 
-            //PROBLEMA: Adicionar um novo elemento ao final
-            //SOLUÇÃO: Usar método Add()
-            empresas.Add("MusicDot");
-            Imprimir(empresas);
+            cronologia.InsertRange(1, new [] { ataque, guerraClones, vinganca, rebels });
+            Imprimir(cronologia);
 
-            //PROBLEMA: Adicionar vários elementos ao final
-            //SOLUÇÃO: Usar método AddRange()
-            var novasEmpresas = new List<string> { "empresa 1", "empresa 2" };
-            empresas.AddRange(novasEmpresas);
-            Imprimir(empresas);
+            Console.WriteLine("cronologia.Capacity: " + cronologia.Capacity);
+            cronologia.Add(despertar);
+            Imprimir(cronologia);
 
-            //PROBLEMA: adicionar novo elemento na segunda posição
-            //SOLUÇÃO: Usar método Insert
-            var posicao = 2;
-            empresas.Insert(posicao - 1, "Hipsters.Jobs");
-            Imprimir(empresas);
+            var indiceEsperanca = cronologia.IndexOf(esperanca);
+            cronologia.Insert(indiceEsperanca, rogue);
+            Imprimir(cronologia);
 
-            //PROBLEMA: adicionar mais de um elemento na terceira posição
-            //SOLUÇÃO: Usar método InsertRange()
-            empresas.InsertRange(2, new List<string> { "empresa 3", "empresa 4" });
-            Imprimir(empresas);
+            cronologia.Add(ultimo);
+            Imprimir(cronologia);
 
-            #region problemas
-            ///PROBLEMA: Ordenar elementos
-            ///SOLUÇÃO: usar método sort
-            ///PROBLEMA: Ordenar elementos por ordem de tamanho de string
-            ///SOLUÇÃO: usar delegate Comparison e método CompareTo
-            ///PROBLEMA: copiar os 2 últimos elementos para nova lista
-            ///SOLUÇÃO: Usar método GetRange()
-            ///PROBLEMA: clonar a lista
-            ///SOLUÇÃO: passar lista original no construtor da lista
-            ///PROBLEMA: remover um elemento
-            ///SOLUÇÃO: usar método Remove()
-            ///PROBLEMA: remover o último elemento
-            ///SOLUÇÃO: Usar método RemoveAt()
-            ///PROBLEMA: remover os dois últimos elementos
-            ///SOLUÇÃO: Usar RemoveRange
-            #endregion
+            cronologia.Reverse();
+            Imprimir(cronologia);
+            cronologia.Reverse();
+            Imprimir(cronologia);
+
+            var filmesComAtores = new List<Filme>(cronologia);
+            var indiceGuerraClones = filmesComAtores.IndexOf(guerraClones);
+            filmesComAtores.RemoveAt(indiceGuerraClones);
+            filmesComAtores.Remove(rebels);
+            Imprimir(filmesComAtores);
+
+            var trilogiaOriginal = new List<Filme>(filmesComAtores);
+            trilogiaOriginal.RemoveAll((f) => f.Ano > 1983);
+            Imprimir(trilogiaOriginal);
+
+            Console.WriteLine("O primeiro filme da cronologia é " + cronologia[0]);
+            Console.WriteLine("O último filme da cronologia é " + cronologia[cronologia.Count - 1]);
+
+            var ordemAlfabetica = new List<Filme>(filmesComAtores);
+            ordemAlfabetica.Sort();
+            Imprimir(ordemAlfabetica);
+            
+            var ordemLancamento = new List<Filme>(filmesComAtores);
+            ordemLancamento.Sort((f1, f2) => f1.Ano.CompareTo(f2.Ano));
+            Imprimir(ordemLancamento);
+
+            var trilogiaInicial = new Filme[3];
+            ordemLancamento.CopyTo(3, trilogiaInicial, 0, 3);
+            Imprimir(trilogiaInicial);
         }
 
-        private static void Imprimir(List<string> empresas)
+        private static void Imprimir(IEnumerable<Filme> cronologia)
         {
-            //for (int i = 0; i < empresas.Count; i++)
-            //{
-            //    var empresa = empresas[i];
-            //    Console.WriteLine(empresa);
-            //}
-
-            //foreach (var empresa in empresas)
-            //{
-            //    Console.WriteLine(empresa);
-            //}
-
-            empresas.ForEach( (empresa) => 
+            foreach (var filme in cronologia)
             {
-                Console.WriteLine(empresa);
-            });
+                Console.WriteLine(filme);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Filme : IComparable
+    {
+        public Filme(string titulo, int ano)
+        {
+            Titulo = titulo;
+            Ano = ano;
+        }
+
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Filme este = this;
+            Filme outro = obj as Filme;
+
+            if (outro == null)
+            {
+                return -1;
+            }
+            return este.Titulo.CompareTo(outro.Titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"{Titulo} - {Ano}";
         }
     }
 }
