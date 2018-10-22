8b082e57687766eda6f59189d9558168f13e1f7f
Marcelo Oliveira <mclricardo@gmail.com>
2018-10-22 20:27:53 -0200

    1027-CertificacaoCSharpParte6-video6.1-Conjuntos.trec

--- 02.04/depois/Program.cs
+++ 02.04/depois/Program.cs
@@ -16,62 +16,67 @@ namespace _02._03
             var despertar = new Filme("Episódio VII -O Despertar da Força", 2015);
             var rogue = new Filme("Rogue One", 2016);
             var ultimo = new Filme("Episódio VIII: Os Últimos Jedi", 2017);
-            
-            ///SETS = CONJUNTOS
 
-            ///Duas características do Set (conjunto)
-            ///1. não permite duplicidade
-            ///2. os elementos não são mantidos em ordem específica
+            //SETS = CONJUNTOS
 
-            ///declarando set de filmes
-            ///ISet<Filme> filmes = new HashSet<Filme>();
-            ///adicionando: esperanca, imperio, retorno
-            ///filmes.Add(esperanca);
-            ///filmes.Add(imperio);
-            ///filmes.Add(retorno);
+            //Duas características do Set (conjunto)
+            //1. não permite duplicidade
+            //2. os elementos não são mantidos em ordem específica
+
+            //declarando set de filmes
+            ISet<Filme> filmes = new HashSet<Filme>();
+            //adicionando: esperanca, imperio, retorno
+            filmes.Add(esperanca);
+            filmes.Add(imperio);
+            filmes.Add(retorno);
 
             ///Imprime os filmes separados por vírgula
-            ///Imprimir(filmes);
+            Imprimir(filmes);
             
-            ///qual a diferença para uma lista?? vamos ver agora
+            //qual a diferença para uma lista?? vamos ver agora
             
-            ///adicionando: ameaca, ataque, vinganca
-            ///filmes.Add(ameaca);
-            ///filmes.Add(ataque);
-            ///filmes.Add(vinganca);
+            //adicionando: ameaca, ataque, vinganca
+            filmes.Add(ameaca);
+            filmes.Add(ataque);
+            filmes.Add(vinganca);
             
-            ///Imprimir(filmes);
-            ///e a ordem???
-            
-            ///removendo imperio, adicionando despertar
-            ///filmes.Remove(imperio);
-            ///filmes.Add(despertar);
-            
-            ///imprimindo de novo
-            ///Imprimir(filmes);
-            
-            ///adicionando vinganca de novo - não gera erro de duplicidade!
-            ///filmes.Add(vinganca);
-            ///Imprimir(filmes);
-            
-            ///qual a vantagem do set sobre a lista? tempo de pesquisa!
-            ///https: //stackoverflow.com/a/10762995
-            
-            ///desempenho HashSet x List: escalabilidade X memória
-            
-            ///desvantagem: consumo de memória
-            
-            ///ordenando: sort
-            ///filmes.Sort();
-            ///copiando: filmesEmLista
-            ///List<Filme> filmesEmLista = new List<Filme>(filmes);
-            ///ordenando copia
-            ///filmesEmLista.Sort();
-            
-            ///imprimindo copia
-            ///Imprimir(filmesEmLista);
-            
-            ///comparando objetos
+            Imprimir(filmes);
+
+            //e a ordem???
+
+            //removendo imperio, adicionando despertar
+            filmes.Remove(imperio);
+            filmes.Add(despertar);
+
+            //imprimindo de novo
+            Imprimir(filmes);
+
+            //adicionando vinganca de novo - não gera erro de duplicidade!
+            filmes.Add(vinganca);
+            Imprimir(filmes);
+
+            //qual a vantagem do set sobre a lista? tempo de pesquisa!
+            //https://stackoverflow.com/a/10762995
+
+            //desempenho HashSet x List: escalabilidade X memória
+
+            //desvantagem: consumo de memória
+
+            //ordenando: sort
+            //filmes.Sort();
+            //copiando: filmesEmLista
+            List<Filme> filmesEmLista = new List<Filme>(filmes);
+            //ordenando copia
+            filmesEmLista.Sort();
+
+            //imprimindo copia
+            Imprimir(filmesEmLista);
+
+            //verificando se objeto existe
+            Console.WriteLine("O filme Uma Nova Esperança existe? " + filmes.Contains(esperanca));
+            var novaEsperanca = new Filme("Episódio IV -Uma nova esperança", 1977);
+
+            Console.WriteLine("O filme Uma Nova Esperança existe? " + filmes.Contains(novaEsperanca));
         }
 
         private static void Imprimir(IEnumerable<Filme> filmes)
@@ -111,6 +116,26 @@ namespace _02._03
         public override string ToString()
         {
             return $"{Titulo} - {Ano}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Filme outro = obj as Filme;
+            if (outro == null)
+            {
+                return false;
+            }
+
+            return this.Titulo.Equals(outro.Titulo) &&
+                this.Ano.Equals(outro.Ano);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -131496797;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Titulo);
+            hashCode = hashCode * -1521134295 + Ano.GetHashCode();
+            return hashCode;
         }
     }
 }
