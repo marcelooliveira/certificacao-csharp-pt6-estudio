cf097e9d9275363267a1b5c15f5dbdef273d0d78
Marcelo Oliveira <mclricardo@gmail.com>
2018-10-23 20:59:47 -0200

    1027-CertificacaoCSharpParte6-video6.2-Dicionários.trec

--- 02.05/depois/Program.cs
+++ 02.05/depois/Program.cs
@@ -1,32 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02._05
 {
     class Program
     {
+        static Queue<string> pedagio = new Queue<string>();
+
         static void Main(string[] args)
         {
-            ///COLEÇÃO: FILA
-            ///Regra: primeiro que entra é o primeiro que sai
+            //COLEÇÃO: FILA
+            //Regra: primeiro que entra é o primeiro que sai
 
-            ///entrou: van          
-            ///< image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
-            ///entrou: kombi
-            ///< image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
-            ///entrou: guincho
-            ///< image url="$(ProjectDir)\Slides\queue3.png" scale=""/>
-            ///entrou: pickup
-            ///< image url="$(ProjectDir)\Slides\queue4.png" scale=""/>
+            //entrou: van          
+            string veiculo = "van";
+            Enfileirar(veiculo);
+            Imprimir();
 
-            ///carro liberado
-            ///< image url="$(ProjectDir)\Slides\queue5.png" scale=""/>
-            ///carro liberado
-            ///< image url="$(ProjectDir)\Slides\queue6.png" scale=""/>
-            ///carro liberado
-            ///< image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
-            ///carro liberado
-            
-            ///carro liberado
+            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
+            //entrou: kombi
+            Enfileirar("kombi");
+            Imprimir();
+
+            ///<image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
+            //entrou: guincho
+            Enfileirar("guincho");
+            Imprimir();
+            ///<image url="$(ProjectDir)\Slides\queue3.png" scale=""/>
+            //entrou: pickup
+            Enfileirar("pickup");
+            Imprimir();
+
+            ///<image url="$(ProjectDir)\Slides\queue4.png" scale=""/>
+
+            //carro liberado
+            Desenfileirar();
+
+            ///<image url="$(ProjectDir)\Slides\queue5.png" scale=""/>
+            //carro liberado
+            Desenfileirar();
+            ///<image url="$(ProjectDir)\Slides\queue6.png" scale=""/>
+            //carro liberado
+            Desenfileirar();
+            ///<image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
+            //carro liberado
+            Desenfileirar();
+
+            //carro liberado
+            Desenfileirar();
+        }
+
+        private static void Desenfileirar()
+        {
+            var liberado = pedagio.Dequeue();
+            Console.WriteLine("Saiu do pedágio: " + liberado);
+            Imprimir();
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            Console.WriteLine("Entrou no pedágio: " + veiculo);
+            pedagio.Enqueue(veiculo);
+        }
+
+        private static void Imprimir()
+        {
+            Console.WriteLine();
+            Console.WriteLine("CONTEÚDO DA FILA:");
+            foreach (var v in pedagio)
+            {
+                Console.WriteLine(v);
+            }
         }
     }
 }
