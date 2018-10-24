46d8b826f4156b528bed1f8783c1fa8147d3cecc
Marcelo Oliveira <mclricardo@gmail.com>
2018-10-24 17:35:27 -0200

    .

--- 02.05/depois/Program.cs
+++ 02.05/depois/Program.cs
@@ -5,8 +5,6 @@ namespace _02._05
 {
     class Program
     {
-        static Queue<string> pedagio = new Queue<string>();
-
         static void Main(string[] args)
         {
             //COLEÇÃO: FILA
@@ -15,67 +13,8 @@ namespace _02._05
             //TAREFA: Implementar uma fila de pedágio
 
             ///<image url="$(ProjectDir)\Slides\queue0.png" scale=""/>
+            
 
-
-            //entrou: van          
-            string veiculo = "van";
-            Enfileirar(veiculo);
-            Imprimir();
-
-            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
-            //entrou: kombi
-            Enfileirar("kombi");
-            Imprimir();
-
-            ///<image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
-            //entrou: guincho
-            Enfileirar("guincho");
-            Imprimir();
-            ///<image url="$(ProjectDir)\Slides\queue3.png" scale=""/>
-            //entrou: pickup
-            Enfileirar("pickup");
-            Imprimir();
-
-            ///<image url="$(ProjectDir)\Slides\queue4.png" scale=""/>
-
-            //carro liberado
-            Desenfileirar();
-
-            ///<image url="$(ProjectDir)\Slides\queue5.png" scale=""/>
-            //carro liberado
-            Desenfileirar();
-            ///<image url="$(ProjectDir)\Slides\queue6.png" scale=""/>
-            //carro liberado
-            Desenfileirar();
-            ///<image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
-            //carro liberado
-            Desenfileirar();
-
-            //carro liberado
-            Desenfileirar();
-        }
-
-        private static void Desenfileirar()
-        {
-            var liberado = pedagio.Dequeue();
-            Console.WriteLine("Saiu do pedágio: " + liberado);
-            Imprimir();
-        }
-
-        private static void Enfileirar(string veiculo)
-        {
-            Console.WriteLine("Entrou no pedágio: " + veiculo);
-            pedagio.Enqueue(veiculo);
-        }
-
-        private static void Imprimir()
-        {
-            Console.WriteLine();
-            Console.WriteLine("CONTEÚDO DA FILA:");
-            foreach (var v in pedagio)
-            {
-                Console.WriteLine(v);
-            }
         }
     }
 }
